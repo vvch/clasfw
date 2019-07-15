f1d97f89e9aad80232051f017c8ff7bc5db40253
Vitaly Chesnokov <chesn@depni.sinp.msu.ru>
2019-07-15 12:11:01 +0300

    channels in auto-generated content

--- generate_test_content.py
+++ generate_test_content.py
@@ -11,7 +11,7 @@ os.environ.setdefault('WERKZEUG_DEBUG_PIN', 'off') # no PIN for browser debugger
 
 from clasfw.app import create_app
 from clasfw.settings import DevConfig
-from clasfw.models import Base, Model, Amplitude
+from clasfw.models import Base, Model, Amplitude, Channel
 from clasfw.extensions import db
 import numpy as np
 
@@ -27,6 +27,16 @@ with app.test_request_context():
         name="test",
         description="test model with dummy values",
         author="V. Mokeev")
+    c1 = Channel(
+        id=1,
+        name="pi+ n",
+        html=r"&pi;<sup>+</sup>n",
+        tex=r"$\pi^+n$", )
+    c2 = Channel(
+        id=2,
+        name="pi0 p",
+        html=r"&pi;<sup>0</sup>p",
+        tex=r"$\pi^0p$")
 
     w_all = np.linspace(0, 4, 10)
     q2_all = np.linspace(0, 5, 10)
@@ -37,6 +47,7 @@ with app.test_request_context():
             print ("W, Q2 = ", w, q2)
             for cos_theta in cos_theta_all:
                 a = Amplitude(
+                    channel=c1,
                     w=w,
                     q2=q2,
                     cos_theta=cos_theta,
@@ -44,4 +55,5 @@ with app.test_request_context():
                 a.t1 = 3+5j
                 m.amplitudes.append(a)
     db.session.add(m)
+    db.session.add(c2)
     db.session.commit()
