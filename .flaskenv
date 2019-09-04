vvch/clasfw

commit f1c9bf2f5b25ff5aeed8ecad017aa8baa4cdddfc

mysql models compatibility

clasfw/models/base_models.py
@@ -29,9 +29,8 @@ class DictionaryMixin(StatusMixin):
         return cls.__name__.lower() + 's'
 
     priority       = Column(Integer, autoincrement=True)
-    name           = Column(String, nullable=False, unique=True)
-    description    = Column(Text, nullable=False,
-        server_default='')
+    name           = Column(String(255), nullable=False, unique=True)
+    description    = Column(Text)
 
     # __mapper_args__ = {
     #     'order_by': 'priority desc'
@@ -57,9 +56,9 @@ def default_html_value(context):
 
 
 class ExtDictionaryMixin(DictionaryMixin):
-    html           = Column(String, default=default_html_value)
-    html_plain     = Column(String, default=default_html_value)
-    tex            = Column(String)
+    html           = Column(String(255), default=default_html_value)
+    html_plain     = Column(String(255), default=default_html_value)
+    tex            = Column(String(255))
 
     def _repr_html_(self):  # for display_html() function in IPython
         return str(self.html)

clasfw/models/models.py
@@ -49,6 +49,7 @@ class Quantity(DatesMixin, ExtDictionaryMixin, Base):
     @property
     def wu(self):
         return self.with_unit()
+
     @property
     def wu_tex(self):
         return self.with_unit('tex')
@@ -58,7 +59,7 @@ class Quantity(DatesMixin, ExtDictionaryMixin, Base):
 
 
 class Model(DatesMixin, DictionaryMixin, Base):
-    author         = Column(String, nullable=False,
+    author         = Column(String(255), nullable=False,
         default='')
     comment        = Column(Text, nullable=False,
         default='')
@@ -82,7 +83,6 @@ class Amplitude(Base):
     q2             = Column(Float)
     w              = Column(Float)
     cos_theta      = Column(Float)
-
 
     number = 6
 

commit b97d1865f5ae83941205d2eaf6e0bc414ddec90f

temporary: workaround bug introduced in f1c9bf2f;
default environment switched to production

.flaskenv
@@ -1,2 +1,2 @@
-FLASK_APP=manage
-FLASK_ENV=development
+FLASK_APP=clasfw.app:create_app
+FLASK_ENV=production

.gitignore
@@ -1,5 +1,6 @@
 settings_local.py
 .htaccess
+.env
 
 *.sqlite
 *.sqlite3

clasfw/models/base_models.py
@@ -3,7 +3,7 @@ __all__ = [
 ]
 
 from database import Base, Column, \
-        Integer, SmallInteger, String, DateTime, \
+        Integer, SmallInteger, String, TIMESTAMP, \
         Text, LargeBinary, \
         relationship, backref, deferred, func, desc
 from sqlalchemy.ext.declarative import declared_attr
@@ -13,14 +13,25 @@ class StatusMixin:
     id             = Column(Integer, primary_key=True, autoincrement=True)
     status         = Column(Integer, default=0)
 
-
+import sqlalchemy
+from sqlalchemy import text
 class DatesMixin:
-    mdate = Column(DateTime, nullable=False,
-        default=func.now(), server_default=func.now(),
-        onupdate=func.now(), server_onupdate=func.now())
-    cdate = Column(DateTime, nullable=False,
-        default=func.now(), server_default=func.now())
+    # in MySQL should be first TIMESTAMP column
+    mdate = Column(TIMESTAMP, nullable=False,
+        default=func.now(),
+        onupdate=func.now(),
+        server_default=func.now(),
+        # server_default=text(
+        #     "{0} ON UPDATE {0}".format(func.current_timestamp())
+        # ),
+        server_onupdate=sqlalchemy.schema.FetchedValue(),
+    )
+        # server_default=text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+    cdate = Column(TIMESTAMP, nullable=False,
+        default=func.now(),
+        server_default=func.current_timestamp())
     # mdate          = Column(DateTime, onupdate=func.utc_timestamp())
+    # mdate          = Column(DateTime, onupdate=func.current_timestamp())
 
 
 class DictionaryMixin(StatusMixin):
@@ -31,6 +42,8 @@ class DictionaryMixin(StatusMixin):
     priority       = Column(Integer, autoincrement=True)
     name           = Column(String(255), nullable=False, unique=True)
     description    = Column(Text)
+    # description    = Column(Text, nullable=False,
+    #     server_default='')  ##  in MySQL TEXT field can not have default value
 
     # __mapper_args__ = {
     #     'order_by': 'priority desc'

database.py
@@ -7,6 +7,7 @@ Boolean      = db.Boolean
 Integer      = db.Integer
 SmallInteger = db.SmallInteger
 DateTime     = db.DateTime
+TIMESTAMP    = db.TIMESTAMP
 Float        = db.Float
 String       = db.String
 Text         = db.Text

migrations/versions/ce6f5e95b530_initial.py
@@ -1,8 +1,8 @@
 """initial
 
-Revision ID: 05fcb1058ab0
+Revision ID: ce6f5e95b530
 Revises: 
-Create Date: 2019-08-14 04:26:51.705958
+Create Date: 2019-09-04 20:59:20.376323
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '05fcb1058ab0'
+revision = 'ce6f5e95b530'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -21,26 +21,26 @@ def upgrade():
     op.create_table('channels',
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('status', sa.Integer(), nullable=True),
-    sa.Column('mdate', sa.DateTime(), server_default=sa.text('CURRENT_TIMESTAMP'), nullable=False),
-    sa.Column('cdate', sa.DateTime(), server_default=sa.text('CURRENT_TIMESTAMP'), nullable=False),
+    sa.Column('mdate', sa.TIMESTAMP(), server_default=sa.text('CURRENT_TIMESTAMP'), nullable=False),
+    sa.Column('cdate', sa.TIMESTAMP(), server_default=sa.text('CURRENT_TIMESTAMP'), nullable=False),
     sa.Column('priority', sa.Integer(), autoincrement=True, nullable=True),
-    sa.Column('name', sa.String(), nullable=False),
-    sa.Column('description', sa.Text(), server_default='', nullable=False),
-    sa.Column('html', sa.String(), nullable=True),
-    sa.Column('html_plain', sa.String(), nullable=True),
-    sa.Column('tex', sa.String(), nullable=True),
+    sa.Column('name', sa.String(length=255), nullable=False),
+    sa.Column('description', sa.Text(), nullable=True),
+    sa.Column('html', sa.String(length=255), nullable=True),
+    sa.Column('html_plain', sa.String(length=255), nullable=True),
+    sa.Column('tex', sa.String(length=255), nullable=True),
     sa.PrimaryKeyConstraint('id', name=op.f('pk_channels')),
     sa.UniqueConstraint('name', name=op.f('uq_channels_name'))
     )
     op.create_table('models',
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('status', sa.Integer(), nullable=True),
-    sa.Column('mdate', sa.DateTime(), server_default=sa.text('CURRENT_TIMESTAMP'), nullable=False),
-    sa.Column('cdate', sa.DateTime(), server_default=sa.text('CURRENT_TIMESTAMP'), nullable=False),
+    sa.Column('mdate', sa.TIMESTAMP(), server_default=sa.text('CURRENT_TIMESTAMP'), nullable=False),
+    sa.Column('cdate', sa.TIMESTAMP(), server_default=sa.text('CURRENT_TIMESTAMP'), nullable=False),
     sa.Column('priority', sa.Integer(), autoincrement=True, nullable=True),
-    sa.Column('name', sa.String(), nullable=False),
-    sa.Column('description', sa.Text(), server_default='', nullable=False),
-    sa.Column('author', sa.String(), nullable=False),
+    sa.Column('name', sa.String(length=255), nullable=False),
+    sa.Column('description', sa.Text(), nullable=True),
+    sa.Column('author', sa.String(length=255), nullable=False),
     sa.Column('comment', sa.Text(), nullable=False),
     sa.PrimaryKeyConstraint('id', name=op.f('pk_models')),
     sa.UniqueConstraint('name', name=op.f('uq_models_name'))
@@ -48,14 +48,14 @@ def upgrade():
     op.create_table('units',
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('status', sa.Integer(), nullable=True),
-    sa.Column('mdate', sa.DateTime(), server_default=sa.text('CURRENT_TIMESTAMP'), nullable=False),
-    sa.Column('cdate', sa.DateTime(), server_default=sa.text('CURRENT_TIMESTAMP'), nullable=False),
+    sa.Column('mdate', sa.TIMESTAMP(), server_default=sa.text('CURRENT_TIMESTAMP'), nullable=False),
+    sa.Column('cdate', sa.TIMESTAMP(), server_default=sa.text('CURRENT_TIMESTAMP'), nullable=False),
     sa.Column('priority', sa.Integer(), autoincrement=True, nullable=True),
-    sa.Column('name', sa.String(), nullable=False),
-    sa.Column('description', sa.Text(), server_default='', nullable=False),
-    sa.Column('html', sa.String(), nullable=True),
-    sa.Column('html_plain', sa.String(), nullable=True),
-    sa.Column('tex', sa.String(), nullable=True),
+    sa.Column('name', sa.String(length=255), nullable=False),
+    sa.Column('description', sa.Text(), nullable=True),
+    sa.Column('html', sa.String(length=255), nullable=True),
+    sa.Column('html_plain', sa.String(length=255), nullable=True),
+    sa.Column('tex', sa.String(length=255), nullable=True),
     sa.PrimaryKeyConstraint('id', name=op.f('pk_units')),
     sa.UniqueConstraint('name', name=op.f('uq_units_name'))
     )
@@ -91,14 +91,14 @@ def upgrade():
     op.create_table('quantities',
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('status', sa.Integer(), nullable=True),
-    sa.Column('mdate', sa.DateTime(), server_default=sa.text('CURRENT_TIMESTAMP'), nullable=False),
-    sa.Column('cdate', sa.DateTime(), server_default=sa.text('CURRENT_TIMESTAMP'), nullable=False),
+    sa.Column('mdate', sa.TIMESTAMP(), server_default=sa.text('CURRENT_TIMESTAMP'), nullable=False),
+    sa.Column('cdate', sa.TIMESTAMP(), server_default=sa.text('CURRENT_TIMESTAMP'), nullable=False),
     sa.Column('priority', sa.Integer(), autoincrement=True, nullable=True),
-    sa.Column('name', sa.String(), nullable=False),
-    sa.Column('description', sa.Text(), server_default='', nullable=False),
-    sa.Column('html', sa.String(), nullable=True),
-    sa.Column('html_plain', sa.String(), nullable=True),
-    sa.Column('tex', sa.String(), nullable=True),
+    sa.Column('name', sa.String(length=255), nullable=False),
+    sa.Column('description', sa.Text(), nullable=True),
+    sa.Column('html', sa.String(length=255), nullable=True),
+    sa.Column('html_plain', sa.String(length=255), nullable=True),
+    sa.Column('tex', sa.String(length=255), nullable=True),
     sa.Column('unit_id', sa.Integer(), nullable=True),
     sa.ForeignKeyConstraint(['unit_id'], ['units.id'], name=op.f('fk_quantities_unit_id_units')),
     sa.PrimaryKeyConstraint('id', name=op.f('pk_quantities')),